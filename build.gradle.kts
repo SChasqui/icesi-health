bcdb613fb32bd50f0c277b6ae341e5f9bef81592
SChasqui <santiagochasqui11@gmail.com>
2021-03-14 21:42:38 -0500

    API working for consulting, uploading and downloading files

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val ktor_version: String by project
+val kotlin_version: String by project
+val logback_version: String by project
+
+plugins {
+    application
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "com.example"
+version = "0.0.1"
+application {
+    mainClass.set("io.ktor.server.netty.EngineMain")
+}
+
+repositories {
+    mavenLocal()
+    jcenter()
+    maven { url = uri("https://kotlin.bintray.com/ktor") }
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlinx") }
+    maven { url = uri("https://dl.bintray.com/kotlin/exposed") }
+}
+
+dependencies {
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+    implementation("io.ktor:ktor-html-builder:$ktor_version")
+    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.4")
+    implementation("org.jetbrains.exposed:exposed:0.8.5")
+    implementation(group = "mysql", name = "mysql-connector-java", version = "6.0.6")
+    implementation(group = "org.slf4j", name = "slf4j-simple", version = "1.7.25")
+    implementation(group = "com.google.code.gson", name = "gson", version = "2.8.1")
+    testImplementation(group = "junit", name = "junit", version = "4.12")
+}
---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,119 @@
+package com.example
+
+import com.google.gson.Gson
+import io.ktor.application.*
+import io.ktor.http.*
+import io.ktor.http.ContentDisposition.Companion.File
+import io.ktor.http.content.*
+import io.ktor.response.*
+import io.ktor.request.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import kotlin.text.get
+
+object Files : Table("files") {
+    val id = integer("id")
+    val name = varchar("name", length = 30)
+    val path = varchar("path",length=50)
+    val type = varchar("type",length=30)
+}
+data class MyFiles(val id: Int, val name: String, val path: String, val type: String)
+
+fun initDB() {
+    val url = "jdbc:mysql://root:password@localhost:3306/icesihealth?useUnicode=true&serverTimezone=UTC"
+    val driver = "com.mysql.cj.jdbc.Driver"
+    Database.connect(url, driver)
+}
+
+fun getFilesData(): String {
+    var json: String = ""
+    transaction {
+        val res = Files.selectAll().orderBy(Files.id, false)
+        val c = ArrayList<MyFiles>()
+        for (f in res) {
+            c.add(MyFiles(id = f[Files.id], name = f[Files.name], path = f[Files.path], type = f[Files.type]))
+        }
+        json = Gson().toJson(c);
+    }
+    return json
+}
+
+fun saveFileInDB(pName: String, pType: String) {
+    transaction {
+        Files.insert {
+            it[name] = pName
+            it[path] = "/test/"
+            it[type] = pType
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    initDB()
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText(getFilesData(), ContentType.Text.Plain)
+            }
+
+            get("/{name}") {
+                // get filename from request url
+                val filename = call.parameters["name"]!!
+                // construct reference to file
+                // ideally this would use a different filename
+                val file = java.io.File("./uploads/$filename")
+                //Force browser to download insteda of prompt
+                call.response.header("Content-Disposition", "attachment; filename=\"${file.name}\"")
+                if(file.exists()) {
+                    call.respondFile(file)
+                }
+                else call.respond(HttpStatusCode.NotFound)
+            }
+
+
+            post("/uploads"){
+                // retrieve all multipart data (suspending)
+                val multipart = call.receiveMultipart()
+                var nameResponse: String = ""
+                var typeResponse: String = ""
+                multipart.forEachPart { part ->
+                    // if part is a file (could be form item)
+                    if(part is PartData.FileItem) {
+                        // retrieve file name of upload
+                        val name = part.originalFileName!!
+                        
+                        val file = java.io.File("./uploads/$name")
+                        nameResponse = name
+                        typeResponse = name.substringAfterLast('.', "")
+
+                        // use InputStream from part to save file
+                        part.streamProvider().use { its ->
+                            // copy the stream to the file with buffering
+                            file.outputStream().buffered().use {
+                                // note that this is blocking
+                                its.copyTo(it)
+                            }
+                        }
+                    }
+                    // make sure to dispose of the part after use to prevent leaks
+                    part.dispose()
+                }
+                saveFileInDB(nameResponse, typeResponse)
+                call.respondText("Archivo cargado exitosamente", ContentType.Text.Plain)
+            }
+
+
+        }
+
+        routing{
+
+        }
+    }.start(wait = true)
+}
+
